a015f604c08717d1164b8859322bc74dcfab2968
Matvey Ralchik <matvey.ralchik@auriga.com>
2019-06-25 15:43:06 +0300

    Lombok added

--- src/main/java/pro/matvey/scheduler/event/Event.java
+++ src/main/java/pro/matvey/scheduler/event/Event.java
@@ -1,5 +1,6 @@
 package pro.matvey.scheduler.event;
 
+import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Entity;
@@ -9,6 +10,7 @@ import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
+@Data
 @Entity
 public class Event {
 
@@ -28,57 +30,6 @@ public class Event {
     @AssertTrue(message = "End date must be after start date")
     public boolean isDateTimeValid() {
         return end.isAfter(start);
-    }
-
-    protected Event() {
-    }
-
-    public Event(String name, LocalDateTime start, LocalDateTime end) {
-        this.name = name;
-        this.start = start;
-        this.end = end;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDateTime getStart() {
-        return start;
-    }
-
-    public void setStart(LocalDateTime start) {
-        this.start = start;
-    }
-
-    public LocalDateTime getEnd() {
-        return end;
-    }
-
-    public void setEnd(LocalDateTime end) {
-        this.end = end;
-    }
-
-    @Override
-    public String toString() {
-        return "Event{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", start=" + start.toString() +
-                ", end=" + end +
-                '}';
     }
 
 }
